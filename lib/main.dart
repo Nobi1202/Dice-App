cc5ec8e0eac1776d0ccbdaf01dd4b494307a76f7
DevTranVanNguyen <nobitavvvvvv@gmail.com>
2021-11-15 08:53:42 +0700

    start building app

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Dice App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const DicePage(),
+    );
+  }
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        centerTitle: true,
+        title: const Text("Dice"),
+      ),
+      body: Container(),
+    );
+  }
+}
